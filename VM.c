5cc64d65a200d65b3823b8523c253648eaa8d9cf
Russel <rjmeeker3@gmail.com>
2024-02-19 12:29:07 -0500

    error checking in VM

--- VM.c
+++ VM.c
@@ -4,5 +4,20 @@
 #include "encodings.h"
 
 int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: ./VM <.bin file> <.tape file>\n");
+        return 1;
+    }
 
+    FILE* bin_file = fopen(argv[1], "r");
+    if (bin_file == NULL) {
+        fprintf(stderr, "ERROR: .bin file failed to open.");
+        return 1;
+    }
+
+    FILE* tape_file = fopen(argv[2], "r");
+    if (tape_file == NULL) {
+        fprintf(stderr, "ERROR: .tape file failed to open.");
+        return 1;
+    }
 }
